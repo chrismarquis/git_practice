3dbb25adeb1ea6798a71178b62cf5d8431350566
chrismarquis <darkmarquis@hotmail.com>
2016-11-30 19:31:41 +0000

    wrestler homework for week 2

--- week2/wrestler.rb
+++ week2/wrestler.rb
@@ -7,3 +7,16 @@
 #[info] You will notice is that gets adds a newline
 #character to the end of the returned string.
 #You can use the chomp method to remove this.
+
+
+puts "tell me the name of your first pet"
+
+pet = gets.chomp.capitalize
+
+puts "what was the name of the street you first lived on"
+
+#improvement - only take the first word regexp mibbes?
+
+street = gets.chomp.capitalize
+
+puts "get your mask on and get ready to rumble as #{pet} #{street}. "
